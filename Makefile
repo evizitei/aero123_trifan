4fdfa7f469fb7f44e4c6d3242c5420ed21d58542
Ethan Vizitei <evizitei@instructure.com>
2019-06-05 14:38:24 -0500

    build command loop

--- Makefile
+++ Makefile
@@ -3,14 +3,18 @@ SRCDIR := src
 BUILDDIR := build
 BINDIR := bin
 TESTDIR := test
+BINNAME := trifan
 
 build:
-	$(CC) -o $(BINDIR)/trifan $(SRCDIR)/TrifanHarness.cpp $(SRCDIR)/FlightController.cpp
+	$(CC) -o $(BINDIR)/$(BINNAME) $(SRCDIR)/TrifanHarness.cpp $(SRCDIR)/FlightController.cpp
 
 clean:
 	@echo " Cleaning..."; 
 	rm $(BUILDDIR)/*
 	rm $(BINDIR)/*
+
+run:
+	$(BINDIR)/$(BINNAME)
 
 test:
 	$(CC) -o $(TESTDIR)/bin/system_tests $(TESTDIR)/TrifanTest.cpp $(SRCDIR)/FlightController.cpp
--- src/TrifanHarness.cpp
+++ src/TrifanHarness.cpp
@@ -4,8 +4,18 @@
 
 int main()
 {
-    std::cout << "Hello, PX4!\n";
+    std::cout << "Trifan initializing...\n";
     FlightController ctrl;
-    std::cout << ctrl.getStatus() << "\n";
+    std::cout << ctrl.getStatus() << "\n" << "...Initialized!\n";
+    std::string flight_command;
+    while(true) {
+        std::cout << "Input command...\n";
+        std::getline(std::cin, flight_command);
+        std::cout << "Executing, " << flight_command << "\n";
+        if(flight_command == "shutdown"){
+            std::cout << "Aborting...\n";
+            break;
+        }
+    }
     return 0;
 }
